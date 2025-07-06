18d3a4725dd87584eefd83698750b00d45e8328b
Qarni657 <qarni657@gmail.com>
2025-07-07 02:01:57 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,6 @@
+// Simplified for mobile use
+import React from 'react';
+function App() {
+  return <h1 style={{textAlign:'center', marginTop: '2em'}}>Demo Crypto Exchange App</h1>;
+}
+export default App;
